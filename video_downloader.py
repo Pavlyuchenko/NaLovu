253ba96591ec6ece8f4b7457f3b4a94cf079b113
Pavlyuchenko <michaelg.pavlicek@gmail.com>
2024-06-24 18:14:04 +0200

    Auto questions merge

--- video_downloader.py
+++ video_downloader.py
@@ -21,6 +21,11 @@ def main():
     # remove all items whose order already exists in /epizody/{order}.mp4
     for item in data:
         if (os.getcwd() + f'/home/pavlyuchenko/Desktop/NaLovu/epizody/{item["order"]}.mp4').split('/')[-1] in os.listdir('/home/pavlyuchenko/Desktop/NaLovu/epizody/'):
+            print(item)
+            print(os.getcwd() + f'/home/pavlyuchenko/Desktop/NaLovu/epizody/{item["order"]}.mp4').split('/')[-1])
+            print(os.listdir('/home/pavlyuchenko/Desktop/NaLovu/epizody/'))
+            print("----")
+            
             data.remove(item)
 
 
